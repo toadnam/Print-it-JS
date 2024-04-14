ca83158b6185467ea5b75d50a296b59fcc4e2001
bridgte <audrey.poissonneau@gmail.com>
2024-04-14 09:25:41 +0200

    ajout des commentaires du code manquants

--- assets/script.js
+++ assets/script.js
@@ -20,9 +20,8 @@ const slides = [
 //eventlistener sur chacune des fleches du carrousel
 let arrow_left = document.querySelector(".arrow_left")
 let arrow_right = document.querySelector(".arrow_right")
-
+//Récupérer le class dots
 let dots = document.querySelector(".dots")
-
 //définir l'index actuel du carousel
 let currentIndex = 0
 //Récupérer l'image à l'intérieur de la div
@@ -36,26 +35,35 @@ let bannerText = document.querySelector("#banner p")
 
 //fonction permettant de mettre à jour le carousel
 function carousel(){
+	//mettre à jour l'image
 	bannerImg.src = slides[currentIndex].image
+	//mettre à jour le texte
 	bannerText.innerHTML = slides[currentIndex].tagLine
 	//Mettre à jours les dots
 	upDateDots()
 }
 
+//fonction créant les bullet points pour chaque image du carousel
 function createDots(){
+	//création de la liste de bullet points
 	let ul = document.createElement("ul")
-	ul.classList.add("dots") // Ajout de la classe dots à ul
+	// Ajout de la classe dots à ul
+	ul.classList.add("dots")
 	dots.appendChild(ul)
-
+	//boucle permettant de récupérer le nombre de bullet points à ajouter
 	for(let i=0; i < slides.length; i++){
 		let li = document.createElement("li")
 		ul.appendChild(li)
-		li.classList.add("dot") // Ajout de la classe dot à tous les li
+		// Ajout de la classe dot à tous les li
+		li.classList.add("dot") 
 	}
 }
 
+//fonction permettant de mettre à jour les bullet points en fonction de l'image à l'écran
 function upDateDots (){
+	// Récupérer toutes les class dot
 	let allDot = document.querySelectorAll(".dot")
+	// Appliquer la class dol_selected au bon bullet point
 	allDot.forEach((dot, index) => {
 		if (index === currentIndex) {
 			dot.classList.add("dot_selected")
@@ -65,15 +73,18 @@ function upDateDots (){
 	})
 }
 
+//ajout du déclenchement de l'image suivant au click sur la flèche de droite
 arrow_right.addEventListener("click",() => {
 	currentIndex = (currentIndex + 1) % slides.length;
 	carousel()
 })
 
+//ajout du déclenchement de l'image suivant au click sur la flèche de gauche
 arrow_left.addEventListener("click",() => {
 	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
 	carousel()
 })
 
+//Appeler les fonctions
 createDots()
 carousel()
