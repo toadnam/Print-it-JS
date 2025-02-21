b481b62612987321b30f27b1b99b227f12a852cc
Melodie <melodie.migault@gmail.com>
2025-02-21 19:06:01 +0100

    défilement ok flèche gauche

--- assets/script.js
+++ assets/script.js
@@ -23,24 +23,43 @@ const slides = [
 ]
 
 let index = 0
-let index2 = 4
 rightArrow.addEventListener("click", () => {
     	console.log("Vous avez cliqué sur la flèche droite")
-		index++
-		console.log(index)
-		imageBanner.src = "./assets/images/slideshow/"+slides[index].image
-		texteBanner.innerHTML = slides[index].tagLine
-		dotsBanner[index].classList.add("dot_selected")
-		dotsBanner[index-1].classList.remove("dot_selected")
+		if (index < 3) {
+			index++
+			dotsBanner[index].classList.add("dot_selected")
+			dotsBanner[index-1].classList.remove("dot_selected")
+			imageBanner.src = "./assets/images/slideshow/"+slides[index].image
+			texteBanner.innerHTML = slides[index].tagLine
+			console.log(index+" if")
+		}else{
+			index = 0
+			dotsBanner[3].classList.remove("dot_selected")
+			dotsBanner[0].classList.add("dot_selected")
+			imageBanner.src = "./assets/images/slideshow/"+slides[0].image
+			texteBanner.innerHTML = slides[0].tagLine
+			console.log(index+"else")
+		}	
 	})
+
 leftArrow.addEventListener("click", () => {
     	console.log("Vous avez cliqué sur la flèche gauche")
-		index2--
-		console.log(index2)
-		imageBanner.src = "./assets/images/slideshow/"+slides[index2].image
-		texteBanner.innerHTML = slides[index2].tagLine
-		dotsBanner[index].classList.remove("dot_selected")
-		dotsBanner[index2].classList.add("dot_selected")
-		dotsBanner[index2+1].classList.remove("dot_selected")
+		if (index2 > 0) {
+			index2--
+			dotsBanner[index].classList.remove("dot_selected")
+			dotsBanner[index2].classList.add("dot_selected")
+			dotsBanner[index2+1].classList.remove("dot_selected")
+			imageBanner.src = "./assets/images/slideshow/"+slides[index2].image
+			texteBanner.innerHTML = slides[index2].tagLine	
+			console.log(index2+" if")
+		}
 	})
+
+
+	// 
+
+	// if(i < nbElements - 4)
+    //     i++;
+    // else
+    //     i = 0;
 
